4a05ce7885de6d60a04244d751bba77bb4643e45
cromz22 <tt.pfr4343@gmail.com>
2022-03-17 20:36:40 +0900

    change dataset to use divided wav files

--- src/mustcv2_dataset.py
+++ src/mustcv2_dataset.py
@@ -1,13 +1,9 @@
 import torch
 from torch.utils.data import Dataset
 from pathlib import Path
-from typing import Tuple
 import yaml
-from itertools import groupby
 import soundfile as sf
 from transformers import Wav2Vec2Processor
-
-from utils.audio_utils import get_waveform
 
 
 class MuSTCv2Dataset(Dataset):
@@ -17,76 +13,69 @@ class MuSTCv2Dataset(Dataset):
     def __init__(self, root: str, tgt_lang: str, split: str) -> None:
         assert split in self.SPLITS and tgt_lang in self.TGT_LANGUAGES
         _root = Path(root) / f"en-{tgt_lang}" / "data" / split
-        wav_root, txt_root = _root / "wav", _root / "txt"
-        assert _root.is_dir() and wav_root.is_dir() and txt_root.is_dir()
+        assert _root.is_dir()
+        wav_root = _root / "wav" / "divided"
+        txt_root = _root / "txt"
+        assert wav_root.is_dir() and txt_root.is_dir()
 
-        # Load audio segments
+        # read yaml file as a list of dicts
         with open(txt_root / f"{split}.yaml") as f:
-            segments = yaml.load(f, Loader=yaml.BaseLoader)
+            dicts = yaml.load(f, Loader=yaml.BaseLoader)
 
-        # Load source and target utterances
+        # load source and target utterances and add to dicts
         src = "en"
         tgt = tgt_lang
         for lang in [src, tgt]:
             with open(txt_root / f"{split}.{lang}") as f:
-                utterances = [r.strip() for r in f]
-            assert len(segments) == len(utterances)
-            for i, u in enumerate(utterances):
-                segments[i][lang] = u
+                utts = [r.strip() for r in f]
+            assert len(dicts) == len(utts)
+            for i, utt in enumerate(utts):
+                dicts[i][lang] = utt
 
-        # Gather info
+        # construct self.data
         self.data = []
-        for wav_filename, _seg_group in groupby(segments, lambda x: x["wav"]):
-            wav_path = wav_root / wav_filename
-            sample_rate = sf.info(wav_path.as_posix()).samplerate
-            seg_group = sorted(_seg_group, key=lambda x: x["offset"])
-            for i, segment in enumerate(seg_group):
-                offset = int(float(segment["offset"]) * sample_rate)
-                n_frames = int(float(segment["duration"]) * sample_rate)
-                _id = f"{wav_path.stem}_{i}"
-                self.data.append(
-                    (
-                        wav_path.as_posix(),
-                        offset,
-                        n_frames,
-                        sample_rate,
-                        segment[src],
-                        segment[tgt],
-                        segment["speaker_id"],
-                        tgt,
-                        _id,
-                    )
-                )
+        counter = 1
+        current_wavfile = ""
+        for dict_elem in dicts:
+            stem = dict_elem["wav"].split(".")[0]
+            uid = f"{stem}_{counter}"
+            wav_path = wav_root / f"{uid}.wav"
 
+            self.data.append(
+                (wav_path.as_posix(), dict_elem[src], dict_elem[tgt], tgt, uid)
+            )
+
+            # assume yaml file is sorted
+            if current_wavfile == dict_elem["wav"]:
+                counter = 1
+            else:
+                counter += 1
+
+        # processor to use in __getitem__
         self.processor = Wav2Vec2Processor.from_pretrained(
             "facebook/wav2vec2-base-960h"
         )
 
     def __getitem__(self, idx: int):  # Dict[np.ndarray, torch.Tensor, torch.Tensor]
         """
-        single input
+        Return single input to model
         """
         (
             wav_path,
-            offset,
-            n_frames,
-            sr,
             src_utt,
             tgt_utt,
-            spk_id,
             tgt_lang,
             utt_id,
         ) = self.data[idx]
-        # waveform, _ = get_waveform(wav_path, frames=n_frames, start=offset)
-        # waveform = torch.from_numpy(waveform).squeeze()
-        # return waveform, sr, src_utt, tgt_utt, spk_id, tgt_lang, utt_id
         waveform, sr = sf.read(wav_path)
 
         sr = torch.tensor([sr])
 
         with self.processor.as_target_processor():
             # target is src_utt because the task is ASR
-            labels = self.processor(src_utt, return_tensors="pt", padding=True).input_ids.squeeze()
+            labels = self.processor(
+                src_utt, return_tensors="pt", padding=True
+            ).input_ids.squeeze()
 
         return dict(
             waveform=waveform,
